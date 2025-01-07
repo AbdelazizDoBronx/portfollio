d61d7803423fbc20755b307e323c06bfe926539e
AbdelazizDoBronx <abdelzizaitmhnd8@gmail.com>
2025-01-07 23:36:59 +0100

    Portfolio and services sections complited

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -8,10 +8,10 @@ const Portfolio = () => {
         <h3 className='text-sm mb-21'>My Portfoloio</h3>
         <h4 className='text-5xl font-medium mb-3'>My Latest Work</h4>
         <p className='text-sm text-neutral-700 text-pretty text-center mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aperiam est ab dolor ipsam atque suscipit! Est dolorum non id.</p>
-        <div className='flex gap-5'>
+        <div className='flex  gap-5'>
             {workData.map((work,idx)=>(
                  <div style={{backgroundImage : `url(${work.bgImage})`}} key={idx} className='bg-no-repeat  bg-contain px-5 py-20 rounded-md flex-1'>
-                    <div className='mt-14 flex gap-3 items-center bg-white px-3 py-5 rounded-lg shadow-md'>
+                    <div className=' mt-14 flex gap-3 items-center bg-white px-3 py-5 rounded-lg shadow-md'>
                         <div>
                             <h3 className='text-nowrap text-2xl font-medium'>{work.title}</h3>
                             <p className='text-sm mt-2'>{work.description}</p>
